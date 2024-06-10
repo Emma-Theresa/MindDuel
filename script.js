5205c62471e69bafd93b595b1c033142d914872b
RithikAR25 <arikothanrithik@gmail.com>
2024-06-10 10:02:10 +0530

    added button

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const player2 = document.getElementById("player2");
     const sprite1 = document.getElementById("Character_sprite1");
     const sprite2 = document.getElementById("Character_sprite2");
+    const task_container = document.querySelector(".task_container");
+    const start_button = document.getElementById("start_button");
 
     // Initially hide player1 and player2
     player1.style.display = 'none';
     player2.style.display = 'none';
+    task_container.style.display = 'none';
 
     startButton.addEventListener("click", () => {
         // Show player1 and player2
+        start_button.style.display = 'none';
         player1.style.display = 'block';
         player2.style.display = 'block';
 
@@ -41,13 +45,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     // Define the green_attack function
-    function green_attack() {
+    function green_attack_r() {
         sprite1.style.top = '10px';
         sprite1.style.left = '-160px';
         sprite1.classList.add("animate_combat_green");
     }
 
-    function blue_attack() {
+    function blue_attack_l() {
         sprite2.style.top = '-87px';
         sprite2.style.left = '-160px';
         sprite2.classList.add("animate_combat_blue");
